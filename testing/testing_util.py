6019a0df6322d000d841f22eee29687bd7776948
Flippyfun02 <amiebinan@gmail.com>
2025-06-18 18:22:09 -0400

    implemented helper functions (for future use)

---
+++ testing/testing_util.py
@@ -0,0 +1,23 @@
+import pytest
+from util import to_float, to_mixed_num
+
+def test_to_float():
+    assert to_float("1") == 1.0
+    assert to_float("0") == 0.0
+    assert to_float("½") == to_float("0.5") == 0.5
+    assert to_float("1½") == to_float("1 ½") == 1.5
+    assert to_float("12½") == 12.5
+    assert to_float("1/2") == 0.5
+    assert to_float("1 1/2") == 1.5
+    assert to_float("11/2") == 5.5
+    with pytest.raises(ValueError):
+        assert to_float("")
+        assert to_float("½½")
+        assert to_float("1/2½")
+        assert to_float("1/2 ½")
+
+def test_to_mixed_num():
+    assert to_mixed_num(1.5) == "1 1/2"
+    assert to_mixed_num(0.5) == "1/2"
+    assert to_mixed_num(0.125) == "1/8"
+    assert to_mixed_num(1) == "1"
---
+++ util.py
@@ -0,0 +1,45 @@
+import re
+from unicodedata import numeric
+from fraction import Fraction
+
+def to_float(str) -> float:
+    """
+    Converts numeric string and/or unicode to a float
+    """
+    if not str:
+        raise ValueError(f"Numeric string '{str}' must be a fraction or mixed number")
+    sum = 0
+    # Considers mixed fraction parts separately
+    for s in re.split(r" |([\u00BC-\u00BE\u2150-\u215E])", str, maxsplit=1):
+        if not s:
+            continue
+        try:
+            # For normal float representations (ex: '0.5')
+            sum += float(s)
+            continue
+        except ValueError:
+            try:
+                # For fractional representations (ex: '1/2')
+                numerator, denominator = s.split("/")
+                sum += float(numerator) / int(denominator)
+                continue
+            except ValueError:
+                try:
+                    # For unicode (ex: '½')
+                    sum += numeric(s)
+                except TypeError:
+                    raise ValueError(f"Numeric string '{str}' must be a fraction or mixed number")
+    return sum
+
+def to_mixed_num(decimal:float) -> str:
+    """Turns decimal into a string mixed number"""
+    frac = Fraction(decimal)
+    whole = frac.numerator // frac.denominator
+    remainder = frac - whole
+
+    if remainder == 0:
+        return str(whole)
+    elif whole == 0:
+        return f"{remainder.numerator}/{remainder.denominator}"
+    else:
+        return f"{whole} {remainder.numerator}/{remainder.denominator}"
