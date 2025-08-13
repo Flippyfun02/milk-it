98bfd91faab2e53a0568b3de8ba6ca67d9784c7f
Flippyfun02 <amiebinan@gmail.com>
2025-08-13 13:31:16 -0400

    styled ingredient list

--- static/js/script.js
+++ static/js/script.js
@@ -1,36 +1,44 @@
 const goBtn = document.getElementById("add-recipe");
 const input = document.getElementById("recipe-url");
 
+// when user adds recipe
 goBtn.addEventListener("click", async () => {
-    if (input === document.activeElement) {
-        const recipe_url = document.getElementById("recipe-url")
-        let url = recipe_url.value;
+    const recipe_url = document.getElementById("recipe-url")
+    let url = recipe_url.value;
 
-        // send POST request to FastAPI
-        const response = await fetch("/add-recipe", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                recipe_url: url
-            })
-        });
-        const data = await response.json();
-        const input_error = document.getElementById("input-error")
-        input_error.textContent = ""
+    // send POST request to FastAPI
+    const response = await fetch("/add-recipe", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            recipe_url: url
+        })
+    });
+    const data = await response.json();
+    const input_error = document.getElementById("input-error")
+    input_error.textContent = ""
 
-        if (data.code === 2) {
-            input_error.textContent = "Please enter a recipe link!"
+    // validate data
+    if (data.code === 2) {
+        input_error.textContent = "Please enter a recipe link!"
+    }
+    else if (data.code === 3) {
+        input_error.textContent = "Unable to find recipe :("
+    }
+    else {
+        // reveal ingredient container
+        document.getElementById("ingredient-container").hidden = false;
+        const ingredientList = document.getElementById("ingredient-list");
+        // display list of items
+        for (index in data.items) {
+            let row = document.createElement("li");
+            row.textContent = data.items[index];
+            ingredientList.appendChild(row);
         }
-        else if (data.code === 3) {
-            input_error.textContent = "Unable to find recipe :("
-        }
-        else {
-            ingredients = document.getElementById("ingredients");
-            ingredients.textContent = JSON.stringify(data.items);
-            recipe_url.value = "";
-        }
+        // ingredients.textContent = JSON.stringify(data.items);
+        recipe_url.value = "";
     }
 });
 
